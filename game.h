882561c76e81886cd3033a6242c0bd10a1cc9264
Léon <leon.geide@mail.de>
2018-02-14 12:19:37 +0100

    russian test

--- game.h
+++ game.h
@@ -1,12 +1,18 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "russian.h"
+
 class game {
   private:
   
   public:
     game() {
       cout << "Hi!" << endl;
+    }
+    
+    void initialize() {
+      russian aparatschik("Nestor Aparatschik", 1874);
     }
 };
 
